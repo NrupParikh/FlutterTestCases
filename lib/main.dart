6c0204194ba7d81c234dc82b416cd8b2ea3e1880
NrupParikh <108717119+NrupParikh@users.noreply.github.com>
2024-07-03 11:04:43 +0530

    Mock Testing using MockClient : Unit Testing Part2

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,31 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/unit_test_class/album.dart';
+import 'package:http/http.dart' as http;
+
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+late final Future<Album> futureAlbum;
+@override
+  void initState() {
+    super.initState();
+    
+    // Call API
+    futureAlbum = fetchAlbum(http.Client());
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,56 +35,31 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:Scaffold(
+      appBar: AppBar(        
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Fetch Data From API"),
+      ),
+      body: Center(
+        child: FutureBuilder<Album>(future: futureAlbum, builder:(context,snapshot){
+          if(snapshot.hasData){
+            return Text(snapshot.data!.title);
+          }else if(snapshot.hasError){
+            return Text("${snapshot.error}");
+          }
+
+          return const CircularProgressIndicator();} ,)
+      ),),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(        
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-        
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),    );
+// ================ Get Album from API
+Future<Album> fetchAlbum(http.Client client) async{
+  final response = await client.get(Uri.parse("https://jsonplaceholder.typicode.com/albums/1"));
+  if(response.statusCode == 200){
+    return Album.fromJson(jsonDecode(response.body) as Map<String,dynamic>);
+  }else {
+    throw Exception("Fail to load Album");
   }
 }
